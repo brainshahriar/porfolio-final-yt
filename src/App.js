1254a4eb5168877c6f2d3d7d51dbddeb62a0389c
shahriarbjit <shahriar.mehedi@bjitgroup.com>
2023-06-03 14:44:47 +0600

    portfolio start

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
+import Portfolio from "./components/Portfolio/Portfolio";
 import Resume from "./components/Resume/Resume";
 import Services from "./components/Services/Services";
 import Sidebar from "./components/Sidebar/Sidebar";
@@ -16,6 +17,7 @@ function App() {
               <About />
               <Services/>
               <Resume/>
+              <Portfolio/>
               <Contact/>
               </div>
                 
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './portfolio.scss'
+
+const Portfolio = () => {
+    return (
+        <section className='work container section' id='work'>
+            
+        </section>
+    );
+};
+
+export default Portfolio;
